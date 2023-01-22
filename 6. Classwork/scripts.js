5413976cc045a5db11387fd121a72973191369f2
Subin Shakya <subs@Subins-MacBook-Pro.local>
2023-01-22 08:08:26 +0545

    answers for classwork

--- 6. Classwork/scripts.js
+++ 6. Classwork/scripts.js
@@ -4,19 +4,38 @@ let books = {
     isFree: true,
  };
 
-books.isFree = false;
+// books.isFree = false;
 
-console.log(books);
+// console.log(books);
 
- books.title.push('Ikigai');
+//  books.title.push('Ikigai');
 
-console.log(books);
+// console.log(books);
 
-books.author.push('Frances Mirales');
+// books.author.push('Frances Mirales');
 
-console.log(books);
+// console.log(books);
 
-Object.assign(books, {language: 'English'});
-// books['language'] = 'English';
+// Object.assign(books, {language: 'English'});
+// // books['language'] = 'English';
 
-console.log(books);
+// console.log(books);
+
+
+// for(let i = 0; i < books.title.length; i++) {
+//     console.log(books.title[i]);
+// }
+
+
+// for(let i=0;i<books.title.length;i++){
+//     console.log(books.title[i]+" was written by "+books.author[i]);
+// }
+
+function areaOfCircle(radius) {
+    const pi = 3.1415;
+    let area = pi * radius * radius;
+    console.log("The area of circle is " + area);
+    console.log(`The area of circle is ${area}`);
+}
+
+areaOfCircle(10);
